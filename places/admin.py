cf1a7a32443706fdccff864d5002e8ba4b404a5e
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2022-12-16 02:38:39 +0330

    revise place models

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,40 @@
-from django.contrib import admin
-
-# Register your models here.
+# from django.contrib import admin
+# from .models import (LocationType, RoomType, AccommodationType,
+#                      Accommodation, Room, AccommodationAttribute,
+#                      RoomAttribute, DatePrice)
+#
+#
+# @admin.register(LocationType)
+# class LocationTypeAdmin(admin.ModelAdmin):
+#     model = LocationType
+#     search_fields = ['title']
+#     list_display = ['id', 'title', 'is_valid', 'create_date']
+#
+#
+# @admin.register(RoomType)
+# class RoomTypeAdmin(admin.ModelAdmin):
+#     model = RoomType
+#     search_fields = ['title']
+#     list_display = ['id', 'title', 'is_valid', 'create_date']
+#
+#
+# @admin.register(AccommodationType)
+# class AccommodationTypeAdmin(admin.ModelAdmin):
+#     model = AccommodationType
+#     search_fields = ['title']
+#     list_display = ['id', 'title', 'is_valid', 'create_date']
+#
+#
+# @admin.register(Accommodation)
+# class AccommodationAdmin(admin.ModelAdmin):
+#     search_fields = ['title']
+#     list_display = ['id', 'title', 'base_price', 'owner', 'standard_capacity', 'build_size']
+#     list_filter = ['owner', 'location_type', 'accommodation_type',
+#                    'standard_capacity', 'maximum_capacity']
+#
+#
+# @admin.register(Room)
+# class RoomAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'title', 'room_type', 'accommodation']
+#     list_filter = ['room_type']
+#
--- places/models.py
+++ places/models.py
@@ -1,14 +1,87 @@
+import datetime
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-import datetime
+
 from users.models import User
 
 
-class LocationType(models.Model):
+
+class BaseModel(models.Model):
     title = models.CharField(verbose_name=_('title'), max_length=64)
     is_valid = models.BooleanField(verbose_name=_('is valid'), default=True)
     create_time = models.DateTimeField(verbose_name=_('date join'), auto_now_add=True)
     last_modify = models.DateTimeField(verbose_name=_('last modify'), auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class DatePrice(models.Model):
+    is_reserve = models.BooleanField(verbose_name=_('is reserve'), default=False)
+    date = models.DateField(verbose_name=_('date'))
+    price = models.FloatField(verbose_name=_('price'))
+
+    class Meta:
+        abstract = True
+
+
+class Attribute(BaseModel):
+    description = models.CharField(verbose_name=_('description'), max_length=64, blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Location(models.Model):
+    country = models.CharField(verbose_name=_('country'), max_length=64)
+    city = models.CharField(verbose_name=_('city'), max_length=64)
+
+    class Meta:
+        db_table = 'location'
+        verbose_name = _('location')
+        verbose_name_plural = _('locations')
+
+
+class Place(BaseModel):
+    HOTEL_TYPE = 1
+    MOTEL_TYPE = 2
+    HOSTEL_TYPE = 3
+    VILLA_TYPE = 4
+    HOLIDAY_CAMP_TYPE = 5
+    ROOM_TYPE = 6
+    PLACE_TYPE = ((HOSTEL_TYPE, _('hotel')),
+                  (MOTEL_TYPE, _('motel')),
+                  (HOSTEL_TYPE, _('hostel')),
+                  (VILLA_TYPE, _('vila')),
+                  (HOLIDAY_CAMP_TYPE, _('holiday camp')),
+                  (ROOM_TYPE, _('room'))
+                  )
+
+    location = models.ForeignKey(Location, models.DO_NOTHING, related_name='place',
+                                 verbose_name=_('location'))
+    address = models.TextField(verbose_name=_("address"))
+    place_type = models.PositiveSmallIntegerField(verbose_name=_('place_type'), choices=PLACE_TYPE)
+    description = models.TextField(verbose_name=_('description'))
+
+    class Meta:
+        db_table = 'place'
+        verbose_name = _('place')
+        verbose_name_plural = _('places')
+
+
+class Option(BaseModel):
+    price = models.FloatField(verbose_name=_('price'), default=0)
+    place = models.ForeignKey(Place, models.CASCADE, verbose_name=_('place'))
+
+    class Meta:
+        db_table = 'option'
+        verbose_name = _('option')
+        verbose_name_plural = _('options')
+
+
+class LocationType(BaseModel):
+    pass
 
     class Meta:
         db_table = 'location_type'
@@ -16,11 +89,8 @@ class LocationType(models.Model):
         verbose_name_plural = _('locations type')
 
 
-class RoomType(models.Model):
-    title = models.CharField(verbose_name=_('title'), max_length=64)
-    is_valid = models.BooleanField(verbose_name=_('is valid'), default=True)
-    create_time = models.DateTimeField(verbose_name=_('date join'), auto_now_add=True)
-    last_modify = models.DateTimeField(verbose_name=_('last modify'), auto_now=True)
+class RoomType(BaseModel, models.Model):
+    pass
 
     class Meta:
         db_table = 'room_type'
@@ -28,11 +98,8 @@ class RoomType(models.Model):
         verbose_name_plural = _('rooms type')
 
 
-class AccommodationType(models.Model):
-    title = models.CharField(verbose_name=_('title'), max_length=64)
-    is_valid = models.BooleanField(verbose_name=_('is valid'), default=True)
-    create_time = models.DateTimeField(verbose_name=_('date join'), auto_now_add=True)
-    last_modify = models.DateTimeField(verbose_name=_('last modify'), auto_now=True)
+class AccommodationType(BaseModel):
+    pass
 
     class Meta:
         db_table = 'accommodation_type'
@@ -40,12 +107,10 @@ class AccommodationType(models.Model):
         verbose_name_plural = _('accommodations type')
 
 
-class Accommodation(models.Model):
-    title = models.CharField(verbose_name=_('title'), max_length=64)
+class Accommodation(BaseModel):
     owner = models.ForeignKey(User, models.CASCADE,
                               related_name='Accommodation', verbose_name=_(_('Accommodation'))
                               )
-    is_valid = models.BooleanField(verbose_name=_('is valid'), default=True)
     base_price = models.FloatField(verbose_name=_('base price'))
     extra_person_price = models.FloatField(verbose_name=_('extra person price'))
     standard_capacity = models.IntegerField(verbose_name=_('standard capacity'))
@@ -68,10 +133,12 @@ class Accommodation(models.Model):
         verbose_name_plural = _('accommodations')
 
 
-class Room(models.Model):
-    title = models.CharField(verbose_name=_('title'), max_length=64)
+class Room(BaseModel):
+    Place = models.ForeignKey(Place, models.CASCADE, 'room', parent_link=True, verbose_name=_('room'))
     accommodation = models.ForeignKey(Accommodation, models.CASCADE,
-                                      related_name='room', verbose_name=_('accommodation'))
+                                      related_name='room', verbose_name=_('accommodation'),
+                                      blank=True, null=True)
+    size = models.IntegerField(verbose_name=_('size'))
     description = models.TextField(verbose_name=_('description'))
     room_type = models.ManyToManyField(RoomType, related_name='room', verbose_name=_('room type'))
 
@@ -79,15 +146,6 @@ class Room(models.Model):
         db_table = 'room'
         verbose_name = _('room')
         verbose_name_plural = _('rooms')
-
-
-class Attribute(models.Model):
-    title = models.CharField(verbose_name=_('title'), max_length=64, blank=True, null=True)
-    is_valid = models.BooleanField(verbose_name=_('is valid'), default=True)
-    description = models.CharField(verbose_name=_('description'), max_length=64, blank=True, null=True)
-
-    class Meta:
-        abstract = True
 
 
 class AccommodationAttribute(Attribute):
@@ -112,14 +170,19 @@ class RoomAttribute(Attribute):
         verbose_name_plural = _('rooms attribute')
 
 
-class DatePrice(models.Model):
-    accommodation = models.ForeignKey(Accommodation, models.CASCADE, related_name='date_price',
-                                      verbose_name=_('accommodation'))
-    is_reserve = models.BooleanField(verbose_name=_('is reserve'), default=False)
-    date = models.DateField(verbose_name=_('date'))
-    price = models.FloatField(verbose_name=_('price'))
+class AccommodationDatePrice(DatePrice):
+    accommodation = models.ForeignKey(Accommodation, models.CASCADE, 'accommodation', verbose_name=_('accommodation'))
 
     class Meta:
-        db_table = 'date_price'
-        verbose_name = _('date price')
-        verbose_name_plural = _('dates price')
+        db_table = 'accommodation_date_price'
+        verbose_name = _('accommodation date price')
+        verbose_name_plural = _('accommodation dates price')
+
+
+class RoomDatePrice(DatePrice):
+    room = models.ForeignKey(Place, models.CASCADE, 'accommodation', verbose_name=_('accommodation'))
+
+    class Meta:
+        db_table = 'room_date_price'
+        verbose_name = _('room date price')
+        verbose_name_plural = _('rooms dates price')
