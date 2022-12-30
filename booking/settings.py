d6f4e9c0eb6bcf9dc9dff98dc64a250b5ef99478
k-roozbahani <kavehrozbah@gmail.com>
2022-12-30 20:42:53 +0330

    add redis to setting

--- booking/settings.py
+++ booking/settings.py
@@ -81,6 +81,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://username:password@127.0.0.1:6379',
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
