336adbfe3ea3ae7033a81e03c0793e46f5e88cbb
k-roozbahani <kavehrozbah@gmail.com>
2022-12-31 18:11:11 +0330

    add currency_choice to redis_utils

--- utils/redis_utils.py
+++ utils/redis_utils.py
@@ -2,17 +2,15 @@ from redis import Redis
 from django.conf import settings
 
 redis_cli = Redis()
+hash_name = settings.REDIS_CURRENCY_HASH_NAME
 
 
 def get_currency(currency_id):
-    hash_name = settings.REDIS_CURRENCY_HASH_NAME
-    print(currency_id, hash_name)
     currency = redis_cli.hget(hash_name, currency_id)
     return currency
 
 
 def set_currency(currency_key, currency_national_symbol):
-    hash_name = settings.REDIS_CURRENCY_HASH_NAME
     exist = get_currency(currency_key)
     if exist and currency_key == exist:
         print('Redis currency already was update')
@@ -24,3 +22,12 @@ def set_currency(currency_key, currency_national_symbol):
         print('add new currency to redis')
         redis_cli.hset(hash_name, currency_key, currency_national_symbol)
         return True
+
+
+def currency_choices():
+    currencies = redis_cli.hgetall(hash_name)
+    list_currencies = []
+    for key, value in currencies.items():
+        currency = (key, value)
+        list_currencies.append(currency)
+    return tuple(list_currencies)
