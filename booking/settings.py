f8f6f095e07986d6f5032918f580a5307078570b
k-roozbahani <kavehrozbah@gmail.com>
2023-01-12 22:27:29 +0330

    add filter to setting

--- booking/settings.py
+++ booking/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
     'rest_framework',
+    'django_filters',
     'users',
     'places',
     'flying',
@@ -164,7 +165,8 @@ REDIS_EXCHANGE_RATE_HASH_NAME = "Exchange_rate"
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 SIMPLE_JWT = {
