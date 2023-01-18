21aec9f4aa7be594ea4631bc1eb9f4a7d2fffd80
k-roozbahani <kavehrozbah@gmail.com>
2023-01-18 22:19:11 +0330

    add new ciew order class

--- carts/views.py
+++ carts/views.py
@@ -1,14 +1,19 @@
 from rest_framework.generics import GenericAPIView, ListAPIView, RetrieveAPIView, CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from rest_framework import viewsets
+from rest_framework.decorators import action
+
+from .replace_models import Order
 from .utils import taking_order, reserve_date_price, get_all_or_one_order, validate_payment, get_object_order
 from places.models import PlaceDatePrice
-from .serializers import PlaceOrderSerializer
+from .serializers import PlaceOrderSerializer, OrderSerializer
 from .models import PlaceOrder
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework import status
 from django.utils.translation import gettext_lazy as _
+from rest_framework.parsers import JSONParser
 
 
 class ReserveViewSet(APIView):
@@ -39,6 +44,7 @@ class ReserveViewSet(APIView):
 
 class PaymentView(APIView):
     permission_classes = [IsAuthenticated]
+
     def get(self, request, pk):
         try:
             order = get_object_order(request, pk)
@@ -59,3 +65,21 @@ class PaymentView(APIView):
             return Response({'status': 'Successful payment'})
         else:
             return Response({'status': 'Unsuccessful payment'})
+
+
+class OrderView(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticated]
+    parser_classes = [JSONParser]
+    serializer_class = OrderSerializer
+
+    @action(methods='post', detail=True, url_path='orders/')
+    def create(self, request):
+        serializer = OrderSerializer(data=request.date)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_queryset(self):
+        return self.request.user.orders.all()
