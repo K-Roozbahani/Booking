87dbf04b2de757c0a616698ef3d86b82110789a8
k-roozbahani <kavehrozbah@gmail.com>
2023-01-19 10:42:37 +0330

    defind order router in carts/urls.py

--- carts/urls.py
+++ carts/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import ReserveViewSet, PaymentView
+from .views import OrderView
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('', ReserveViewSet.as_view(), name='reserve'),
-    path('<int:pk>/', ReserveViewSet.as_view(), name='detail_reserve'),
-    path('payment/<int:pk>/', PaymentView.as_view(), name='payment'),
-    path('payment/resolve/', PaymentView.as_view(), name='payment'),
-]
+router = DefaultRouter()
+router.register(r'order', OrderView, basename='order')
+urlpatterns = router.urls
