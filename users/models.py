3918860ef2a8edd4aff925280ef8ac36e9b10f32
k-roozbahani <kavehrozbah@gmail.com>
2023-01-19 17:18:34 +0330

    delete all migrates and make new migrate

--- users/models.py
+++ users/models.py
@@ -4,11 +4,11 @@ from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
 
 
+
 class User(AbstractUser):
-    phone_number = models.CharField(verbose_name=_('phone number'), max_length=16,
-                                    validators=[RegexValidator(
-                                        regex=r'^\+?1?\d{9,15}$',
-                                        message="Phone number must be entered in the format '+123456789'. Up to 15 digits allowed."
-                                    )])
+    message = "Phone number must be entered in the format '+123456789'. Up to 15 digits allowed."
+
+    phone_number = models.CharField(verbose_name=_('phone number'), max_length=16, unique=True,
+                                    validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$', message=message)])
     USERNAME_FIELD = "phone_number"
     REQUIRED_FIELDS = []
