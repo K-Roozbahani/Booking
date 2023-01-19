dc6b76ead52081ec62a20e025e6c68d0fbdce761
k-roozbahani <kavehrozbah@gmail.com>
2023-01-19 16:02:38 +0330

    restore otp  login again

--- users/views.py
+++ users/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from .models import User
+from django.core.cache import cache
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+
+def otp_generator(*args, **kwargs):
+    return 1234567890
+
+
+def send_sms_to_user(*args, **kwargs):
+    pass
+
+
+class LoginByOtpView(viewsets.ViewSet):
+
+    @action(methods='post', detail=True, url_path='user/login/get_phone_number')
+    def get_phone_number(self, request):
+        if request.data.get('phone'):
+            otp = otp_generator()
+            cache.set(request.data['phone'], otp, 60 * 2)
+
+        else:
+            data = {"error": "phone field required"}
+            return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
+
+        send_sms_to_user.delay(request.data['phone'], otp)
+
+        return Response(data={'phone': request.data.get('phone')}, status=status.HTTP_200_OK)
+
+    @action(methods='post', detail=True, url_path='user/login/get_otp/')
+    def get_otp(self, request):
+        if request.data.get('phone') and request.data.get('otp'):
+            phone = request.data['phone']
+            otp = request.data["otp"]
+
+            user_otp = cache.get(phone)
+
+            if user_otp == otp:
+                try:
+                    user = User.objects.get(phone=phone)
+                except User.DoesNotExist:
+                    user = User.objects.create_user(phone=phone)
+
+                refresh = RefreshToken.for_user(user)
+                context = {
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token),
+                }
+                return Response(context)
+            else:
+                raise AuthenticationFailed("{} invalid otp".format(request.data["otp"]))
+
+        else:
+            data = {"error": "phone and otp fields required"}
+            return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
