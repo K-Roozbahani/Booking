9ab9c50f37fde271df2040fc214d921f3b9bf491
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2023-01-20 20:00:13 +0330

    change gunicorn log neme

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,7 @@ loglevel = 'debug'
 workers = 2
 bind = '0.0.0.0:8000'
 
-accesslog = '/var/log/gunicorn/imdb.log'
-errorlog = '/var/log/gunicorn/imdb.log'
+accesslog = '/var/log/gunicorn/booking.log'
+errorlog = '/var/log/gunicorn/booking.log'
 
 capture_output = True
